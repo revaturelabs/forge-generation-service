12099806ba6de2b09aace2eb6c782f39e9c73e52
gabriela <gabskatzedev@gmail.com>
2020-10-15 16:56:45 -0400

    application core and development database

---
+++ Portfolio-Generation-Service/src/main/java/com/forge/PortfolioGenerationService/PortfolioGenerationServiceApplication.java
@@ -0,0 +1,15 @@
+package com.forge.PortfolioGenerationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class PortfolioGenerationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioGenerationServiceApplication.class, args);
+	}
+
+}
