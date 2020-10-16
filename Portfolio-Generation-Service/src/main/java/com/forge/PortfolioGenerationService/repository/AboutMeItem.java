e87a33d79bc618d5f5bba2f68527020850d8bab4
johncamores <37806717+johncamores@users.noreply.github.com>
2020-10-16 16:56:25 -0400

    blocker somewhat solved

--- Portfolio-Generation-Service/src/main/java/com/forge/PortfolioGenerationService/repository/AboutMeItem.java
+++
@@ -1,7 +0,0 @@
-package com.forge.PortfolioGenerationService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AboutMeItem extends JpaRepository<AboutMeItem , Integer> {
-
-}
--- Portfolio-Generation-Service/src/main/java/com/forge/PortfolioGenerationService/repository/EducationRespository.java
+++
@@ -1,9 +0,0 @@
-package com.forge.PortfolioGenerationService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.forge.PortfolioGenerationService.models.Education;
-
-public interface EducationRespository extends JpaRepository<Education , Integer>{
-
-}
