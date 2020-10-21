9ad9980dd7c5ad5d4e19d1b8fe1d33c6b8939138
vyruss <vyruss@gmail.com>
2020-10-21 16:42:16 -0500

    renamed to PDF generation service

--- Portfolio-Generation-Service/src/main/java/com/forge/PDFGenerationService/PortfolioGenerationServiceApplication.java
+++ Portfolio-Generation-Service/src/main/java/com/forge/PDFGenerationService/PortfolioGenerationServiceApplication.java
@@ -1,4 +1,4 @@
-package com.forge.PortfolioGenerationService;
+package com.forge.PDFGenerationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
