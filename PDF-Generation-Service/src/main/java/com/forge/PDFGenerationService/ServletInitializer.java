34ebcbfbc4d9be5ba20afe569fcc88ebb4b432b0
Aldo <caballero.aldo97@gmail.com>
2020-10-27 16:21:11 -0700

    war file for PDF-Generation-Service

---
+++ PDF-Generation-Service/src/main/java/com/forge/PDFGenerationService/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.forge.PDFGenerationService;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+		@Override
+		protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    		return application.sources(PDFGenerationServiceApplication.class);
+		}
+}
