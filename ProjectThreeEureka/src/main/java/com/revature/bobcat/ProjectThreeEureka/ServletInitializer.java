bb27f8fd5f4721f9197ee36ff1bf5c90ef637e60
Morgen Lanueva <morgen.lanueva@gmail.com>
2020-10-28 16:00:22 -0500

    adding Eureka war

--- ProjectThreeEureka/src/main/java/com/revature/bobcat/ProjectThreeEureka/ServletInitializer.java
+++ ProjectThreeEureka/src/main/java/com/revature/bobcat/ProjectThreeEureka/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.forge.PDFGenerationService;
+package com.revature.bobcat.ProjectThreeEureka;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,6 +7,6 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 		@Override
 		protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    		return application.sources(PDFGenerationServiceApplication.class);
+    		return application.sources(ProjectThreeEurekaApplication.class);
 		}
 }
